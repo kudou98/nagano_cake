a4d940a730a8fb00901b89e1859c292588630bae
kudou98 <fanh210mon@gmail.com>
2022-04-26 15:32:37 +0000

    [Add]カラム追加

--- db/migrate/20220426133723_devise_create_customers.rb
+++ db/migrate/20220426133723_devise_create_customers.rb
@@ -4,8 +4,18 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
   def change
     create_table :customers do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.boolean :is_deleted,        default: true, null: false
+      # 会員ステータスカラム TRUE =有効　FALSE =退会済
+      t.string :last_name,          null: false, default: ""
+      t.string :first_name,         null: false, default: ""
+      t.string :last_name_kana,     null: false, default: ""
+      t.string :first_name_kana,    null: false, default: ""
+      t.string :email,              null: false, default: "", unique: true
       t.string :encrypted_password, null: false, default: ""
+      t.string :postal_code,        null: false, default: ""
+      t.string :address,            null: false, default: ""
+      t.string :telephone_number,   null: false, default: ""
+      
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,16 @@ ActiveRecord::Schema.define(version: 2022_04_26_133732) do
   end
 
   create_table "customers", force: :cascade do |t|
+    t.boolean "is_deleted", default: true, null: false
+    t.string "last_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name_kana", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "postal_code", default: "", null: false
+    t.string "address", default: "", null: false
+    t.string "telephone_number", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
