fd29b384945000ca55074a05cbae5e38258e71fe
kudou98 <fanh210mon@gmail.com>
2022-04-30 04:12:31 +0000

    [Add]管理者ログイン画面

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,18 @@ Rails.application.routes.draw do
 
 # 顧客用
 # URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
+devise_for :customers, controllers: {
+  sessions: 'public/sessions',
+  passwords: 'public/passwords',
+  registrations: "public/registrations"
 }
 
 # 管理者用
 # URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
+devise_for :admin, controllers: {
+  sessions: "admin/sessions",
+  passwords: 'admin/passwords',
+  registrations: 'admin/registrations'
 }
 
 end
