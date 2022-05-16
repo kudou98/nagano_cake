52c9a517e90d118080bb4f8a8b57b49d8e6ca919
kudou98 <fanh210mon@gmail.com>
2022-05-16 14:09:38 +0000

    修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :customer
 	has_many   :order_details, dependent: :destroy
-	enum status: [:入金待ち, :入金確認, :製作中, :発送準備中, :発送済み]
-	enum payment: [:クレジットカード, :銀行振込]
+	enum status: { "入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4 }
+	enum payment: { "クレジットカード": 0, "銀行振込": 1 }
 
   after_update do
     if self.status == "入金確認"
